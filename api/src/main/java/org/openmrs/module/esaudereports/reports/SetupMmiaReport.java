38e4314a5d5f9e45d695298dbdd5a8be5abb785e
Guimino <minoneves@gmail.com>
2018-04-09 15:57:02 +0200

    added second dataSetDefinition

--- api/src/main/java/org/openmrs/module/esaudereports/reports/SetupMmiaReport.java
+++ api/src/main/java/org/openmrs/module/esaudereports/reports/SetupMmiaReport.java
@@ -16,7 +16,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class SetupMmiaReport extends EsaudeDataExportManager {
-
+	
 	@Override
 	public ReportDefinition constructReportDefinition() {
 		ReportDefinition rd = new ReportDefinition();
@@ -28,86 +28,87 @@ public class SetupMmiaReport extends EsaudeDataExportManager {
 		rd.addDataSetDefinition("mmia2", Mapped.mapStraightThrough(mmia2DataSetDefinition()));
 		return rd;
 	}
-
+	
 	@Override
 	public String getDescription() {
-
+		
 		return "Test creating MMIA Report";
 	}
-
+	
 	@Override
 	public String getName() {
-
+		
 		return "MMIA Report";
 	}
-
+	
 	@Override
 	public String getUuid() {
-
+		
 		return "070708b2-2dcf-11e8-afc6-cfac93f9ecaa";
 	}
-
+	
 	@Override
 	public String getVersion() {
 		return "0.1";
 	}
-
+	
 	@Override
 	public String getExcelDesignUuid() {
 		return "e6d1b8b2-2dce-11e8-9c34-6fe694d670c1";
 	}
-
+	
 	@Override
 	public ReportDesign buildReportDesign(ReportDefinition reportDefinition) {
-
+		
 		ReportDesign rd = createExcelTemplateDesign(getExcelDesignUuid(), reportDefinition, "MmiaReport.xls");
 		rd.setName("MMIA");
 		Properties props = new Properties();
 		props.put("repeatingSections", "sheet:1,row:10,dataSet:mmia");
-		props.put("repeatingSections", "sheet:1,row:10,dataSet:mmia2");
 		props.put("sortWeigth", "5000");
 		rd.setProperties(props);
-
+		
 		return rd;
 	}
-
+	
 	private DataSetDefinition mmiaDataSetDefinition() {
 		SqlDataSetDefinition patientDataSetDefinition = new SqlDataSetDefinition();
 		patientDataSetDefinition.setName("mmia");
 		patientDataSetDefinition.setSqlQuery(MmiaSqlSource.SQL_FIND_BALANCETE_BY_PERIOD);
-
+		
 		patientDataSetDefinition.addParameter(new Parameter("startDate", "", Date.class));
 		patientDataSetDefinition.addParameter(new Parameter("endDate", "", Date.class));
-
+		
 		return patientDataSetDefinition;
 	}
-
+	
 	private DataSetDefinition mmia2DataSetDefinition() {
 		SqlDataSetDefinition patientDataSetDefinition = new SqlDataSetDefinition();
 		patientDataSetDefinition.setName("mmia2");
-		patientDataSetDefinition.setSqlQuery("select name from drug");
-
+		patientDataSetDefinition.setSqlQuery("SELECT name as 'DROGA' from drug where name like'%DD%'");
+		patientDataSetDefinition.addParameter(new Parameter("startDate", "", Date.class));
+		patientDataSetDefinition.addParameter(new Parameter("endDate", "", Date.class));
+		
 		return patientDataSetDefinition;
 	}
-
+	
 	@Override
 	public List<Parameter> getParameters() {
-
+		
 		List<Parameter> pList = new ArrayList<Parameter>();
 		pList.add(new Parameter("startDate", "Start Date", Date.class));
 		pList.add(new Parameter("endDate", "End Date", Date.class));
-
+		
 		return pList;
 	}
-
+	
 	@Override
 	public List<ReportDesign> constructReportDesigns(ReportDefinition reportDefinition) {
-
+		
 		List<ReportDesign> reportDesigns = new ArrayList<ReportDesign>();
-
+		
 		reportDesigns.add(buildReportDesign(reportDefinition));
-
+		
 		return reportDesigns;
 	}
-
+	
 }
